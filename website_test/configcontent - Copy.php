364ca66e4a54d42d07623b9b55ff9abfc3c1f51d
sahilsamani <40260218+sahilsamani@users.noreply.github.com>
2019-04-09 16:29:06 +1000

    Final-Work - Just Modified a bit before submitting
    Final-Work - Just Modified a bit before submitting

---
+++ website_test/configcontent - Copy.php
@@ -0,0 +1,10 @@
+<?php
+
+$host       = "localhost";
+$username   = "root";
+$password   = "secret";
+$dbname     = "fitness_tracker_app"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
